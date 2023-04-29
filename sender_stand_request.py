0598488cc231b320b8b14352b3b1dfdf55ad9a62
Luiza <luizabochckareva@yandex.ru>
2023-04-29 07:20:09 +0300

    deleted comments

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -10,14 +10,8 @@ def post_new_order(body):
 
 
 order_track = post_new_order(data.order_body).json()["track"]
-print(order_track)
 
 
 # получение данных о заказе по треку api/v1/orders/track?t=123456
 def get_info_order(track):
     return requests.get(configuration.URL_SERVICE + configuration.TAKE_ORDER_BY_TRACK + str(track))
-
-
-response = get_info_order(order_track)
-print(response.status_code)
-print(response.json())
